498e11e7395179947e73364bf6a2738013600157
nhienoi <nhienduyvu@gmail.com>
2021-06-09 06:25:52 +0700

    muon dit khanh ly

---
+++ bai182.cpp
@@ -0,0 +1,24 @@
+#include <iostream> //thu vien ho tro viec nhap xuat
+using namespace std; //su dung 
+int main() {  //bo xu li  cua chuong trinh, this is main function play an important rule like brain of humen. process 
+	int a; // khai bao bien so nguyen a chua gia tri la so nguyen
+	// cu phap kieu du lieu  tenbien;
+	//quy tac khai bao bien chi chua ki tu a dne chu thuong khon gduo dau bang so 
+	// ten chi co the la chu cai (a, b, c, d, e, f, g)
+	// cac tu khoa khong duoc su dung de khai bao bien
+	// asm, auto enum, signed 
+	// temp hop le
+	//_hoten hop le
+	// gia-tr hop le
+	// double 
+	//ten kieu du lieu ten bien;	
+	char a;
+	int a;
+	int a, b, c, d , e, f ,g, h,i, k;
+	const int a = 9;
+	const double a = 10;
+	const int HEADDICK;
+	
+	system("pause"); // dung man hinh
+	return 0;
+}
