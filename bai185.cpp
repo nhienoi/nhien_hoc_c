f48175121894e42c00724c91e9bdeb8a0ec3e731
nhienoi <nhienduyvu@gmail.com>
2021-06-10 06:58:14 +0700

    update file from local

---
+++ bai185.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int search(int m[], int n, int x) {
+    for (int i = 0; (i < n) && (m[i] != x); i++) {
+        int i;
+        for (
+    }
+
+}
+int main() {
+    int n;
+    cout << "vui long nhap n: ";
+    cin >> n;
+    int m[n];
+    for (int i = 0; i < n; i++) {
+         cin >> m[i];
+    }
+
+
+}
