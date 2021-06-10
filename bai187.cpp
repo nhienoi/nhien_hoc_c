e99625abf5f1e952b2eaeab68a4b3b82b41410e6
nhienoi <nhienduyvu@gmail.com>
2021-06-10 07:24:11 +0700

    uk

---
+++ bai187.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main() {
+	cout << "int: " << sizeof(int) << "byte" << endl;
+	cout << "double: " << sizeof(double) <<"byte" << endl;
+	cout << "short: " << sizeof(short) <<"byte"<< endl;
+	cout << "************************\n";
+	char c;
+	short s;
+	int n;
+	float fVarName;
+	double dVarName3;
+	long double ldVarName3;
+	float fVarName{4.0f};
+	double {4};
+	long double {9l};
+	double dVarName1 {6900000.0};
+	cout << endl;
+	cout << "ki hieu khoa hoc trong lap trinh" << endl;
+	cout << 2,5e9;
+ 	return 0;
+}
