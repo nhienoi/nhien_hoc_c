d97147d7cb0068300cfe744945591ce7f403b4c7
nhienoi <nhienduyvu@gmail.com>
2021-06-15 15:13:13 +0700

    insertion sort

---
+++ bai205.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+
+void type(int m[], int n);
+void epxrt(int m[], int n);
+void insertionSort(int m[], int n);
+
+void type(int m[], int n) {
+	srand(time(NULL));
+	for (int i = 0; i < n; i++) {
+		m[i] = rand() % 100;
+	}
+}
+
+void exprt(int m[], int n) {
+	for (int i = 0; i < n; i++) {
+		cout << "m[" << i << "] = " << m[i] << endl;
+	}
+	cout << endl;
+}
+
+void insertionSort(int m[], int n) {
+	k
+}
+
+using namspace std;
+int main() {
+	int n;
+	cout << "quantity of array: ";
+	cin >> n;
+	int m[n];
+	type(m, n);
+	cout << "original array: " << endl;
+	exprt(m, n);
+	insertionSort(m, n);
+	cout << "array after sorted: " << endl;
+	exprt(m, n);
+	return 0;
+}
