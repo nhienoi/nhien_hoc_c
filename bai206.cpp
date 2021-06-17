e17996360f71249d5a75d0162e971e5c4b812893
nhienoi <nhienduyvu@gmail.com>
2021-06-17 23:50:55 +0700

    them bai 206

---
+++ bai206.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+void type(int m[], int n);
+void result(int m[], int n);
+
+void type(int m[], int n) {
+	for (int i = 0; i < n; i++) {
+		cout << "m[" << i << "] = ";
+		cin >> m[i];
+	}
+}
+
+void result(int m[], int n) {
+	int min;
+	for (int i = 1; i < n; i++) {
+		min = m[0];
+		if (min > m[i]) {
+			temp = min;
+			min = m[i];
+			m[i] = temp;
+		}
+	}
+	if (min > 0) {
+		cout << "gia tri duong nho nhat: " << min;
+	} else {
+		cout << -1;
+	}
+}
+
+int main() {
+	int n;
+	cout << "nhap so luong n: ";
+	cin >> n;
+	int m[n];
+	type(m, n);
+	result(m, n);
+	return 0;
+}
