4099292c432bea6b28456ab02bfda8c1b81ded68
nhienoi <nhienduyvu@gmail.com>
2021-07-08 12:24:23 +0700

    poiter

---
+++ bai216.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+struct people {
+    people(int t, float v1, float cc) {
+        tuoi = t;
+        vong_mot = v1;
+        chieu_cao = cc;
+    }
+    int tuoi;
+    float vong_mot;
+    float chieu_cao;
+};
+
+int main() {
+    people thao(22, 95, 175);
+    people trang(18, 98, 170);
+    people anh(29, 85, 160);
+    cout << &thao;
+    return 0;
+}
